9cbc2d5a9065090a17a39ae2e7c5a8ec0d0a33ed
Adrian Villanueva Echepari <florie.echepari@gmail.com>
2026-03-24 14:47:59 +0800

    Fixed "java.lang.String cannot be cast to java.lang.Boolean" error

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,7 +1,5 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 
 export default function MainLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }} />
-  );
+  return <Slot />;
 }
